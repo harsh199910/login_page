b43fe103acc0cb1af7cd46982c6bc21b2ea0d218
Harsh mishra <41248662+harsh199910@users.noreply.github.com>
2020-01-07 19:30:30 +0530

    Add files via upload

---
+++ login_page.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<h1>login page in html</h1>
+</head>
+<body>
+<form action="arr.php" method="post">
+username <input type ="text" name ="user"><br>
+password <input type ="text" name = "pass"><br>
+<input type ="submit" name ="login">
+</form>
+</body>
+</html>
